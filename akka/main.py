2875cc30ad81daaa351a78a40ed486a6e2a86fee
Liora Milbaum <liora@lmb.co.il>
2025-05-17 17:48:39 +0300

    Environments page

--- akka/main.py
+++ akka/main.py
@@ -1,8 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+import configparser
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    """Function printing Hello, World!"""
-    return "Hello, World!"
+@app.route('/')
+def show_environments():
+    config = configparser.ConfigParser()
+    config.read('environments.cfg')
+    environments = []
+    for section in config.sections():
+        env = {'name': section}
+        env.update(config[section])
+        environments.append(env)
+    return render_template('environments.html', environments=environments)
+
+if __name__ == '__main__':
+    app.run(debug=True)
